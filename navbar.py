5d5c0174834712d8851840cd54c56017006d3c1f
jean-khoury <jeankhoury_98@hotmail.com>
2021-05-17 09:59:53 +0200

    New home page

--- navbar.py
+++ navbar.py
@@ -1,9 +1,6 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-PLOTLY_LOGO = "https://media-exp1.licdn.com/dms/image/C4D0BAQEcaCgbdHgkYQ/company-logo_200_200/0/1605701418054?e" \
-              "=2159024400&v=beta&t=rw6rRDs5W4c281bo7vY6cBLdLZRs70aoIw-z0HgidFg "
-
 navbar = dbc.NavbarSimple(
 
     children=[
