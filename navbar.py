554b74e886dcc16005340fd0b2a9148e4b31b325
jean-khoury <jeankhoury_98@hotmail.com>
2021-05-20 23:30:45 +0200

    removed home page

--- navbar.py
+++ navbar.py
@@ -4,7 +4,6 @@ import dash_html_components as html
 navbar = dbc.NavbarSimple(
 
     children=[
-        dbc.NavItem(dbc.NavLink("Home", href="/home")),
         dbc.NavItem(dbc.NavLink("Builds & Tests Stats", href="/builds-tests-stats")),
         dbc.NavItem(dbc.NavLink("CppCheck", href="/cpp-check")),
         dbc.NavItem(dbc.NavLink("Bazel Stats", href="/bazel-stats")),
